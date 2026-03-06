814c8a71ead8573f9b42ee871af7a665ae74bd1c
Oskar Johnson-Liu <YOUR_GITHUB_EMAIL>
2026-03-06 16:47:18 -0500

    d

--- launchergame.py
+++ launchergame.py
@@ -1,10 +1,11 @@
 import pgzrun
+import random
 
 WIDTH = 800
 HEIGHT = 600
 
 #launcher config
-launcher_x = 100
+launcher_x = 50
 x_movement_launcher = 0
 launcher_y = 300
 y_movement_launcher = 0
@@ -12,12 +13,8 @@ launcher_radius = 30
 launcher_color = "orange"
 
 #projectile config
-projectile_x = 100
-x_movement_projectile = 0
-projectile_y = 300
-y_movement_projectile = 0
-projectile_radius = 10
-projectile_color = "orange"
+bullets = []
+targets = []
 
 # settings:
 mouse_directed_movement = False
@@ -27,29 +24,79 @@ difx = 0
 dify = 0
 movement_sensitivity = 2
 launch = False
-launch_count = 0
+hits = 0
 projectile_vel = 8
 friction = False
 friction_value = 0.05
 gravity = False
 gravitational_accl = 0.05
-time = 0
+times = 0
+
+class Bullet():
+    def __init__(self):
+        self.x = launcher_x
+        self.y = launcher_y
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.radius = 10
+        self.speed = 2
+    def draw_shape(self):
+        screen.draw.filled_circle((self.x, self.y), self.radius, self.color)
+    def fire(self):
+        self.x += self.speed
+    def destroy(self):
+        bullets.remove(self)
+
+class Target():
+    def __init__(self):
+        self.radius = random.randint(20, 30)
+        self.x = random.randint((50+launcher_radius+self.radius), (WIDTH - self.radius))
+        self.y = random.randint(self.radius, (HEIGHT - self.radius))
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def draw_shape(self):
+        screen.draw.filled_circle((self.x, self.y), self.radius, self.color)
+    def destroy(self):
+        targets.remove(self)
+
+def add_target():
+    targets.append(Target())
+
+add_target()
+
+clock.schedule_interval(add_target, 1.5)
 
 def draw():
     screen.clear()
-    screen.draw.text(f"Time: {round(time, 2)}", (50, 100), background = "black", fontsize = 20)
-    screen.draw.text(f"Times Launch: {launch_count}", (50, 500), background = "black", fontsize = 30)
+    for bullet in bullets:
+        bullet.draw_shape()
+    for target in targets:
+        target.draw_shape()
     screen.draw.filled_circle((launcher_x, launcher_y), launcher_radius, launcher_color)
-    screen.draw.filled_circle((projectile_x, projectile_y), projectile_radius, projectile_color)
+    screen.draw.text(f"Time: {round(times, 2)}", (50, 100), background = "black", fontsize = 20)
+    screen.draw.text(f"Hits: {hits}", (50, 500), background = "black", fontsize = 30)
 
 def update():
-    global launcher_x, x_movement_launcher, launcher_y, y_movement_launcher, projectile_x, x_movement_projectile, projectile_y, y_movement_projectile, projectile_radius, launch, gravity, gravitational_accl, friction, friction_value, time, mouse_directed_movement, difx, dify, movement_sensitivity
-    time += 1/60
+    global launcher_x, x_movement_launcher, launcher_y, y_movement_launcher, launch, gravity, gravitational_accl, friction, friction_value, times, mouse_directed_movement, difx, dify, movement_sensitivity, hits
+    times += 1/60
+
+    for bullet in bullets:
+        bullet.fire()
+
+    #check for bullet hitting target
+    for bullet in bullets:
+        for target in targets:
+            distance = ((bullet.x - target.x)**2 + (bullet.y - target.y)**2)**(1/2)
+            if distance <= (target.radius + bullet.radius):
+                hits += 1
+                target.destroy()
+                bullet.destroy()
+    
+    #check for bullet out of screen
+    for bullet in bullets:
+        if bullet.x > (WIDTH + bullet.radius):
+            bullet.destroy()
     
     #mouse directed movement
-    difx = posx - launcher_x
     dify = posy - launcher_y
-    launcher_x += difx * movement_sensitivity/10
     launcher_y += dify * movement_sensitivity/10
 
     #launcher stay inside screen
@@ -62,53 +109,15 @@ def update():
     if launcher_y >= (HEIGHT - launcher_radius):
         launcher_y = (HEIGHT - launcher_radius)
 
-
-    #projectile location
-    if launch == False:
-        gravity = False
-        friction = False
-        projectile_x = launcher_x
-        projectile_y = launcher_y
-    else: #launch == True
-        gravity = True
-        friction = True
-        if projectile_x >= (WIDTH - projectile_radius) or projectile_y >= (HEIGHT - projectile_radius):
-            launch = False
-            x_movement_projectile = 0
-            y_movement_projectile = 0
-
-    #gravity    
-    if gravity == True:
-        y_movement_projectile += gravitational_accl
-
-    #friction
-    if friction == True:
-        print('.  hhhhhldhflkdsa')
-        x_movement_projectile -= friction_value
-        if x_movement_projectile <= 0:
-            x_movement_projectile = 0
-            friction = False 
-
+    
     #update launcher pos
     launcher_x += x_movement_launcher
     launcher_y += y_movement_launcher
 
-    #update pojectile pos
-    projectile_x += x_movement_projectile
-    projectile_y += y_movement_projectile
-
-    print(mouse_directed_movement)
-    print(difx)
-    print(dify)
-    print()
-
 
 def on_mouse_down(button):
-    global x_movement_projectile, launch, projectile_vel, launch_count
-    if button == mouse.LEFT and launch == False:
-        launch = True
-        launch_count += 1
-        x_movement_projectile = projectile_vel
+    global launch, projectile_vel
+    bullets.append(Bullet())
 
 def on_mouse_move(pos):
     global launcher_x, launcher_y, difx, dify, mouse_directed_movement, posx, posy
